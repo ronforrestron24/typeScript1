aec80f1a9d89341b54ee07ec2a9ca0b38f68b0d2
Ron Manguerra <ronforrestron24@gmail.com>
2025-01-16 21:33:45 +0800

    feat: ts practice

--- app.ts
+++ app.ts
@@ -1,10 +1,32 @@
-const add = (n1: number, n2: number, showResult: boolean) => {
-  return n1 + n2;
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "Maximilian",
+//   age: 30,
+// };
+
+// console.log(person.name);
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: "Maximilian",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
+person.role.push("admin");
+// person.role[1] = 10;
 
-const result = add(number1, number2, printResult);
-console.log(result);
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
